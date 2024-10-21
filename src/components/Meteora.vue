b00de178ec123f8c713a4411cc21c55f4c685e41
shypkov <plsql_developer@mail.ru>
2024-10-21 15:48:29 +0300

    update for points checker page

--- src/components/Meteora.vue
+++ src/components/Meteora.vue
@@ -1,24 +1,31 @@
 <script>
 /* eslint-disable */ 
 import axios from 'axios'
-//import { Axios } from 'axios'
 import { useLocalStorage } from '@vueuse/core'
 import { ref } from 'vue'
 
-const meteora1 = useLocalStorage('wallet1', '');
-const meteora2 = useLocalStorage('wallet2', '');
-const meteora3 = useLocalStorage('wallet3', '');
-const meteora4 = useLocalStorage('wallet4', '');
-const meteora5 = useLocalStorage('wallet5', '');
+const wallet1 = useLocalStorage('wallet1', '');
+const wallet2 = useLocalStorage('wallet2', '');
+const wallet3 = useLocalStorage('wallet3', '');
+const wallet4 = useLocalStorage('wallet4', '');
+const wallet5 = useLocalStorage('wallet5', '');
+const wallet6 = useLocalStorage('wallet6', '');
+const wallet7 = useLocalStorage('wallet7', '');
+const wallet8 = useLocalStorage('wallet8', '');
+const wallet9 = useLocalStorage('wallet9', '');
+const wallet10 = useLocalStorage('wallet10', '');
+const wallet11 = useLocalStorage('wallet11', '');
+const wallet12 = useLocalStorage('wallet12', '');
+const wallet13 = useLocalStorage('wallet13', '');
+const wallet14 = useLocalStorage('wallet14', '');
+const wallet15 = useLocalStorage('wallet15', '');
 const resulttext = ref([]);
 let result = ref();
 const apiUrl = ref('https://point-api.meteora.ag/points/');
+const wallets = [wallet1.value, wallet2.value, wallet3.value, wallet4.value, wallet5.value, wallet6.value, wallet7.value, wallet8.value, wallet9.value, wallet10.value, wallet11.value, wallet12.value, wallet13.value, wallet14.value, wallet15.value];
 
 async function GetMeteora() {
   try {
-    // Create an array of wallet addresses
-    const wallets = [meteora1.value, meteora2.value, meteora3.value, meteora4.value, meteora5.value];
-
     // Iterate over wallets and fetch data for each
     for (const wallet of wallets) {
       if (wallet) { // Only fetch if the wallet is not empty
@@ -38,11 +45,22 @@ async function GetMeteora() {
 export default {
   setup () {
   return {
-      meteora1,
-      meteora2,
-      meteora3,
-      meteora4,
-      meteora5,
+      wallet1,
+      wallet2,
+      wallet3,
+      wallet4,
+      wallet5,
+      wallet6,
+      wallet7,
+      wallet8,
+      wallet9,
+      wallet10,
+      wallet11,
+      wallet12,
+      wallet13,
+      wallet14,
+      wallet15,
+      wallets,
       resulttext,
       result,
       apiUrl,
@@ -53,43 +71,43 @@ export default {
 </script>
 
 <template>
-<div style="width: 1200px; margin-left: 20px; font-size: 10px; max-height: auto;">
+<div style="width: 1200px; margin-left: 20px; font-size: 11px; max-height: auto;">
 </div>
 
 <div class="container">
 <div class="row" style="width: 1200px; margin-left: 20px;">
   <q-card>
-  <div class="row" style="width: 1200px; margin-left: 0px;">
-    <div class="col-sm" style="max-width: 380px; margin-left: auto;">
-    <div class="q-gutter-md q-pa-md" style="font-size: 12px;">
-    <ul><b>Meteora points checker</b></ul>
-    <q-input standout="bg-teal text-white" stack-label v-model="meteora1" label="Address1"/>
-    <q-input standout="bg-teal text-white" stack-label v-model="meteora2" label="Address2"/>    
-    <q-input standout="bg-teal text-white" stack-label v-model="meteora3" label="Address3"/> 
-    <q-input standout="bg-teal text-white" stack-label v-model="meteora4" label="Address4"/>
-    <q-input standout="bg-teal text-white" stack-label v-model="meteora5" label="Address5"/>
+  <div class="row" style="width: 1200px; padding: 15px">
+    <div class="col-sm" style="width: 400px; ">
+    <div class="q-gutter-md q-pa-md" style="font-size: 11px;">
+<!--    <ul><b>Points checker</b></ul>-->
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet1" label="Address1"/>
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet2" label="Address2"/>    
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet3" label="Address3"/> 
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet4" label="Address4"/>
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet5" label="Address5"/>
     </div>
 <!--    <q-btn fab icon="add" color="grey" />-->
     </div>
-    <div class="col-sm" style="max-width: 400px; margin-left: auto;">
-    <div class="q-gutter-md" style="width: 400px; margin-left: 0px;  padding: 12px;">
-    <ul><b>Solayer Points checker</b></ul>
-    <q-input disable standout="bg-teal text-white" stack-label v-model="solayer1" label="Address1"/>
-    <q-input disable standout="bg-teal text-white" stack-label v-model="solayer2" label="Address2"/>    
-    <q-input disable standout="bg-teal text-white" stack-label v-model="solayer3" label="Address3"/> 
-    <q-input disable standout="bg-teal text-white" stack-label v-model="solayer4" label="Address4"/>
-    <q-input disable standout="bg-teal text-white" stack-label v-model="solayer5" label="Address5"/>
+    <div class="col-sm" style="max-width: 400px;">
+    <div class="q-gutter-md q-pa-md" style="width: 400px; font-size: 11px; padding: 15px;">
+<!--    <ul><b>Points checker by addresses</b></ul>-->
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet6" label="Address6"/>
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet7" label="Address7"/>    
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet8" label="Address8"/> 
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet9" label="Address9"/>
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet10" label="Address10"/>
     </div>
 <!--    <q-btn fab icon="add" color="grey" />-->
     </div>  
-    <div class="col-sm" style="align-items: center; margin-left: 20px;">
-    <div class="q-gutter-md q-pa-md" style="max-width: 400px; padding: 12px;">
-    <ul><b>Satlayer Points checker</b></ul>
-    <q-input disable standout="bg-teal text-white" stack-label v-model="satlayer1" label="Address1"/>
-    <q-input disable standout="bg-teal text-white" stack-label v-model="satlayer2" label="Address2"/>    
-    <q-input disable standout="bg-teal text-white" stack-label v-model="satlayer3" label="Address3"/> 
-    <q-input disable standout="bg-teal text-white" stack-label v-model="satlayer4" label="Address4"/>
-    <q-input disable standout="bg-teal text-white" stack-label v-model="satlayer5" label="Address5"/>
+    <div class="col-sm" style="max-width: 400px;">
+    <div class="q-gutter-md q-pa-md" style="max-width: 400px; font-size: 11px; padding: 15px;">
+<!--    <ul><b>Satlayer Points checker</b></ul>-->
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet11" label="Address11"/>
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet12" label="Address12"/>    
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet13" label="Address13"/> 
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet14" label="Address14"/>
+    <q-input standout="bg-teal text-white" stack-label v-model="wallet15" label="Address15"/>
     </div>
   </div>
 </div>
