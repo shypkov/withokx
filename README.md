9c5e37542edb398916f5133abafa6c6f9a0f7a0a
shypkov <plsql_developer@mail.ru>
2025-03-01 12:11:46 +0300

    faq and readme final update

--- README.md
+++ README.md
@@ -4,13 +4,18 @@ Can be useful for participating in airdrops and other types of activities.
 Send random amounts between min max with random delay.
 
 *It Doesn't check for invalid adress input. The author is not responsible for any loss of funds.*
+*Please Notice that this is just an command interface for OKX API*
+*there is no interface security risks of losing cex funds, as all the transfers are encypted*
+*all possible securities issues might appear only with client's insecure actions on OKX cex*
 
 ## For manual build and serve:
 # Requirements:
 1. Uses node.js 19.9.0: https://nodejs.org/en/
 2. build on vue and quasar
+3. download the project open terminal and execute sequencely: yarn install, yarn serve (to open the local version)
+4. or create own build by executing: yarn build
 # CLIENT USAGE:
-1. Code deployed on github pages and accesible via https://shypkov.github.io/withokx 
+1. Code deployed on github pages and accesible via https://shypkov.github.io/withokx latest version is 1.3.0
 2. Go to https://www.okx.com/ru/account/my-api and Create API key, secret and passcode.
-3. Сheck the box next to the Enable Withdrawals and configure whitelist for addresses
+3. Сheck the box next to the Enable Withdrawals and configure whitelist of withdrawal addresses
 4. Add the wallets you want to receive funds to the whitelisted
--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -9,16 +9,16 @@
         </q-card-section>
         <q-card-section class="q-pt-none">
           <p>Disclaimer: This App doesn't store any secrets or other keys in cloud\external storages. Clearing the browser\page cache will drop all the stored secrets and other data from input fields</p>
-          <p>- INFO: Withdraw funds conditions and tech usage are different through differen CEX's and you shoould get some knowledge about this difference</p>
+          <p>- INFO: Withdraw funds conditions and tech usage are different through differen CEX's and you should get some knowledge about this difference before the start</p>
           <p>- INFO: This app layout is not adapted for mobile browsers, but still can be used on mobile browsers</p>
           <p>- INFO: Get "Min withdraw Fee" works only if ApiKey, SecretKey and Passphrase are set correctly</p>
-          <p>- Action: Go to <a href='https://www.okx.com/ru/account/my-api'>OKX API management and Create API key</a>. Check the box next to the Enable Withdrawals. You shoud receive in result SecretKey and API key + passphrase for this keys and you need to fill the proper fields in form</p>
+          <p>- Action: Go to <a href='https://www.okx.com/ru/account/my-api'>OKX API management and Create API key</a>. Check the box next to the Enable Withdrawals. You shoud receive in result SecretKey and API key + passphrase for this keys and you need to fill the proper fields in app</p>
           <p>- Action: !WHITELIST all the adresses on exchange management page on which you wish to accept withdrawals. Notice that you need to whitelist adresses depending on networks</p>
           <p>- Action: Fill the fields above with values, optionally you can enable random time delay between transfers on addresses and just click Withdraw button... watch result in console log</p>
           <p>- WARNING: Please DO NOT RELOAD or CLOSE THIS Page when WITHDRAW is Running. If you do - the process will be terminated</p>
           <p>- WARNING: Notice that Form inputs are not validated. Be sure you enter a proper adress and values before withdraw</p>
           <p>- Debug: If you see CORS error in console then install browser extension to avoid CORS blocking <a href='https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf'>CORS EXT</a> and enable it.</p>
-          <p>- Debug: if you see Network error message with "net::ERR_NAME_NOT_RESOLVED" then select another CONNECTION url from the dropdown button CONNECTION</p>
+          <p>- Debug: if you see Network error message with "net::ERR_NAME_NOT_RESOLVED" then this means you couldnot reach the okx.com check your connection</p>
           <p>- ADVICE: if you still have issues with the app or need a neccessary function, contact the dev: <a href='https://t.me/cryptopitek'>cryptopitek</a></p>
         </q-card-section>
         <q-card-actions align="right">
